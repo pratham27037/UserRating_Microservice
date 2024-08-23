389ae0fb57798edc597d2443ba8aacb55c52bc5a
Pratham Choudhary <prathamchoudhary27037@gmail.com>
2024-08-23 11:52:46 +0530

    api security done

---
+++ HotelService/HotelService/src/main/java/com/pratham/hotel/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.pratham.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+
+        security
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {})
+                );
+
+        return security.build();
+    }
+}
